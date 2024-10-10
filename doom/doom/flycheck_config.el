98f86fcef38514e036b27e4e364c0eeb8a3144f6
Andr3wcr0z <aduron2010@gmail.com>
2024-10-10 12:57:59 -0600

    doom emacs config changed (gruvboxs style

--- doom/doom/flycheck_config.el
+++ doom/doom/flycheck_config.el
@@ -53,31 +53,10 @@
                             "#+title: ${title}\n")
          :unnarrowed t)))
 
-setq org-roam-capture-templates
-      '(("P" "PHP" plain
-         "%?"
-         :if-new (file+head "Programming/php/${slug}.org"
-                            "#+title: ${title}\n#+date: %U\n")
-         :unnarrowed t)
-
-setq org-roam-capture-templates
-      '(("j" "Journal" plain
-         "%?"
-         :if-new (file+head "Journal/${slug}.org"
-                            "#+title: ${title}\n#+date: %U\n* Journal")
-         :unnarrowed t)
-
-setq org-roam-capture-templates
-      '(("g" "Genki Note" plain
-         "%?"
-         :if-new (file+head "reference/Genki/${slug}.org"
-                            "#+title: ${title}\n#+date: %U\n")
-         :unnarrowed t)
-
 (setq org-roam-capture-templates
-      '(("p" "Programming" plain
+      '(("p" "Programming Languages" plain
          "%?"
-         :if-new (file+head "Programming/${slug}.org"
+         :if-new (file+head "reference/Programming/${slug}.org"
                             "#+title: ${title}\n#+date: %U\n")
          :unnarrowed t)))
 
